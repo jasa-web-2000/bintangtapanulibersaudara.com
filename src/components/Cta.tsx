7c3b8a7d186ceb053f5e18f1fc93aa778c20655d
dion-zebua <zebuadbless@gmail.com>
2025-08-11 15:05:23 +0700

    malas ngoding, paham 🖐

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -10,10 +10,10 @@ export function Cta() {
       id="cta"
       className="p-8 md:p-16 lg:px-32 bg-gray-800/90 bg-center bg-no-repeat bg-cover rounded-lg bg-blend-multiply bg-[url(/images/cta.jpg)] border-b-8 border-b-indigo-500 md:grid grid-cols-12">
       <div className="md:col-span-8">
-        <p className="text-2xl max-w-xl mb-5 font-extrabold leading-relaxed tracking-wide text-slate-100">
+        <p className="!text-left text-2xl max-w-xl mb-5 font-extrabold leading-relaxed tracking-wide text-slate-100">
           Pesan travel anda sekarang juga sebelum kehabisan slot!
         </p>
-        <p className="max-w-xl mb-5 text-slate-300 md:mb-0">
+        <p className="!text-left max-w-xl mb-5 text-slate-300 md:mb-0">
           Jadwalkan travel Anda hari ini untuk pengalaman perjalanan yang aman,
           cepat, nyaman, dan terpercaya bersama layanan profesional kami.
         </p>
